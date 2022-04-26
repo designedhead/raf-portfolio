eace97b879b36e542a5cd69dfe4052a1d95ca7eb
designedhead <rdesignedhead@gmail.com>
2022-04-26 19:13:54 +0100

    first push

---
+++ src/components/About.js
@@ -0,0 +1,5 @@
+function About() {
+  return <main></main>;
+}
+
+export default About;
---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <main className="hero" id="hero">
+      <h1>Hi, my name is</h1>
+      <h2>Rafael Mendes.</h2>
+      <h3>I build things for the web.</h3>
+      <p>
+        {
+          "I’m a Front End Developer coming from a Product Manager and UI/UX Designer Background. I work with React.js and other hip frameworks."
+        }
+      </p>
+      <button>View Projects</button>
+    </main>
+  );
+}
+
+export default Hero;
