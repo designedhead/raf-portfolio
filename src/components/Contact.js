3e10fc31814af5d1cf81bba2c105269678c02bbb
designedhead <rdesignedhead@gmail.com>
2022-04-28 17:46:22 +0100

    push

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,12 @@ function Contact() {
   return (
     <main className="contact" id="contact">
       <a className="anchor" id="contact_"></a>
-      <h2>What's Next?</h2>
+      <h2>{"What's Next?"}</h2>
       <h3>Get in Touch</h3>
       <p>
-        I'm currently open to new opportunities, my inbox is always open.
-        Weather you have a question or just want to say hi, I'll do my best to
-        get back to you.
+        {
+          " I'm currently open to new opportunities, my inbox is always open. Weather you have a question or just want to say hi, I'll do my best to get back to you."
+        }
       </p>
       <a
         className="button"
