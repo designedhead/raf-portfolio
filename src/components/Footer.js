ed804d5f929ca47043eb746abcbda866db978080
designedhead <rdesignedhead@gmail.com>
2022-04-28 18:14:57 +0100

    smaller package

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,8 @@
-import Moment from "react-moment";
-
 function Footer() {
   const Today = new Date();
 
   return (
     <footer>
-      <p className="footer_info_title">
-        Have an Awesome <Moment format="dddd">{Today}</Moment>
-      </p>
       <a
         className="footer_info"
         href="https://github.com/designedhead"
