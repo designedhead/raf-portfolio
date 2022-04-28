cafa8f4730db9dc9935e9108e033bb89aa19bfa1
designedhead <rdesignedhead@gmail.com>
2022-04-28 18:10:07 +0100

    test for speed

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import About from "../src/components/About";
 import Contact from "../src/components/Contact";
 import Experience from "../src/components/Experience";
-import Footer from "../src/components/Footer";
 import Hero from "../src/components/Hero";
 import Links from "../src/components/Links";
 import Nav from "../src/components/Nav";
-import Projects from "../src/components/Projects";
+//import Projects from "../src/components/Projects";
+//import Footer from "../src/components/Footer";
 import ScrolltoTop from "../src/components/ScrolltoTop";
+
+import dynamic from "next/dynamic";
+
+const Projects = dynamic(() => import("../src/components/Projects"), {
+  loading: () => <p>...</p>,
+});
+const Footer = dynamic(() => import("../src/components/Footer"), {
+  loading: () => <p>...</p>,
+});
 
 export default function Home() {
   return (
