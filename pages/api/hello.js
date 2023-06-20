20eff641cf6d44ea03fa802dcdaaadbba7b36e71
Rafael Mendes <30901317+designedhead@users.noreply.github.com>
2023-06-20 10:26:26 +0100

    api moch

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const orders = [
+    {
+      order_id: "123123",
+      email: "raff@gmail.com",
+      status: {
+        status: "PENDING",
+        message: "Message will go here",
+        timestamp: "2023-06-20T09:12:11.372Z",
+      },
+      shipping: [
+        {
+          courier: "That Courier",
+          tracking_url: "rafael.com",
+          tracking_code: "ABSH134",
+        },
+      ],
+    },
+  ];
+  res.status(200).json({ name: "John Doe", orders });
 }
